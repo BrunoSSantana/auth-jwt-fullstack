c73e3469050fd9bd297586b9dc4da17abe1ae2aa
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-03 14:35:15 -0300

    Add useContext

--- frontend/src/pages/DashBoard.tsx
+++ frontend/src/pages/DashBoard.tsx
@@ -1,15 +1,19 @@
 // import { useState } from 'react'
 // import { useEffect } from 'react'
 // import { api } from '../services/api'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 import '../styles/Home.scss'
 
 
 export function DashBoard() {
+
+  const {user} = useContext(AuthContext)  
     
   return (
     <div>
       <h1>
-        Bruno Santana
+        {user?.name}
       </h1>
     </div>
   )
